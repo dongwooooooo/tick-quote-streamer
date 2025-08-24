a282389effa78e6180af99ed6367d42ee55326f7
dongwoo <woomacho@gmail.com>
2025-08-24 21:21:46 +0900

    feat: config 설정 구현
    - 카프카 파티션 설정
    - 카프카 프로듀서 설정
    - kis 웹소켓 설정

---
+++ collector/src/main/java/org/example/collector/config/KisWebSocketProperties.java
@@ -0,0 +1,38 @@
+package org.example.collector.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "kis")
+public class KisWebSocketProperties {
+    private WebSocket websocket = new WebSocket();
+    private App app = new App();
+    private List<String> targetStocks = List.of(
+        "005930", // 삼성전자
+        "000660", // SK하이닉스  
+        "373220", // LG에너지솔루션
+        "207940", // 삼성바이오로직스
+        "005935", // 삼성전자우
+        "012450", // 한화에어로스페이스
+        "005380", // 현대차
+        "329180", // HD현대중공업
+        "034020", // 두산에너빌리티
+        "105560"  // KB금융
+    );
+
+    @Data
+    public static class WebSocket {
+        private String url;
+    }
+
+    @Data
+    public static class App {
+        private String key;
+        private String secret;
+    }
+}
